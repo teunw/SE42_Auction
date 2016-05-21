33b09388d6005c7009edac6e219269fc4fc9fe43
Teun <teun.willems@live.nl>
2016-05-21 16:55:11 +0200

    Initial commit

---
+++ src/main/java/auction/service/RegistrationMgr.java
@@ -0,0 +1,54 @@
+package auction.service;
+
+import auction.dao.UserDAO;
+import auction.dao.UserDAOCollectionImpl;
+import auction.domain.User;
+
+import java.util.List;
+
+public class RegistrationMgr {
+
+    private UserDAO userDAO;
+
+    public RegistrationMgr() {
+        userDAO = new UserDAOCollectionImpl();
+    }
+
+    /**
+     * Registreert een gebruiker met het als parameter gegeven e-mailadres, mits
+     * zo'n gebruiker nog niet bestaat.
+     *
+     * @param email
+     * @return Een Userobject dat geïdentificeerd wordt door het gegeven
+     * e-mailadres (nieuw aangemaakt of reeds bestaand). Als het e-mailadres
+     * onjuist is ( het bevat geen '@'-teken) wordt null teruggegeven.
+     */
+    public User registerUser(String email) {
+        if (!email.contains("@")) {
+            return null;
+        }
+        User user = userDAO.findByEmail(email);
+        if (user != null) {
+            return user;
+        }
+        user = new User(email);
+        userDAO.create(user);
+        return user;
+    }
+
+    /**
+     * @param email een e-mailadres
+     * @return Het Userobject dat geïdentificeerd wordt door het gegeven
+     * e-mailadres of null als zo'n User niet bestaat.
+     */
+    public User getUser(String email) {
+        return userDAO.findByEmail(email);
+    }
+
+    /**
+     * @return Een iterator over alle geregistreerde gebruikers
+     */
+    public List<User> getUsers() {
+        return userDAO.findAll();
+    }
+}
